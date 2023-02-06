c16a3812d3ea1f9bde4813093fd0dee44bc86271
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-06 18:57:32 +0530

    day02

--- src/Day02/program02.java
+++ src/Day02/program02.java
@@ -1,9 +1,16 @@
 package Day02;
 
 public class program02 {
+	public static long fun()
+	{
+	int x=20;
+	System.out.println(x*x);
+	return x*x;
+	}
 
 	public static void main(String[] args) {
-		
+	double a=fun();
+		System.out.println(a+a);
 
 	}
 
