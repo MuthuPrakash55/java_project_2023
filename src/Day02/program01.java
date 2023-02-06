df191ec8ba36198f5d836ef7215a2850d368c8ba
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-06 18:20:39 +0530

    day02

--- src/Day02/program01.java
+++ src/Day02/program01.java
@@ -1,9 +1,28 @@
 package Day02;
 
 public class program01 {
+	public static void fun()
+	{
+		int x=20;
+		char y='S';
+		byte a=125;
+		short b=31000;
+		long c= 12345678;
+		boolean d=true;
+		double a1;
+		a1=123.456;
+		float a2=123.45f;
+		System.out.println(x*x);
+		System.out.println("Hi");
+		System.out.println(" Good Morning");
+		System.out.println(" Have a nice day");
+	}
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		fun();
+		
+		
 
 	}
 
