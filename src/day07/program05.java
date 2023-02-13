f05c7a8d1b7c1015b1268db500ea250af58fc9df
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-13 11:03:20 +0530

    program05

--- src/day07/program05.java
+++ src/day07/program05.java
@@ -10,13 +10,13 @@ public class program05 {
 	if((a>b)&&(a>c)) {
 		System.out.println("a is greater than b and c");
 	}
-	else if((a<b)&&(b>c)) {
+	else if((b>a)&&(b>c)) {
 		System.out.println("b is greater than a and c");
 	}
-	else if ((a==b)&&(a>c)) {
-		System.out.println("a and b are greater");
+	else if ((a==b)&&(a==c)) {
+		System.out.println("a and b and c are equal");
 	}
-	else {
+	else if ((c>a)&&(c>b)){
 		System.out.println("c is greater than a and b");
 	}
 	System.out.println("program ends");	
