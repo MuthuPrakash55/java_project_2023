364b78b6c8e1aeab1253e29b480db7a222e398c0
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-16 13:37:35 +0530

    day08

--- src/day08/program01.java
+++ src/day08/program01.java
@@ -3,10 +3,10 @@ package day08;
 public class program01 {
 
 	public static void main(String[] args) {
-		int a=1000;
-		int b=40000;
-		int c=50;
-		int d=400;
+		int a=40000;
+		int b=40;
+		int c=40;
+		int d=4;
 	
 		if((a>=b)&&(a>=c)&&(a>=d)) {
 			if(a==b&&a==c&&a==d) {
@@ -69,7 +69,33 @@ public class program01 {
 			else if(b>a&&b>c&&b==d) {
 				System.out.println("b & d are equal & greater than a & c");
 			}
-			
+			else if(a>b&&b==d&&b>c) {
+				System.out.println("a is greater than b and b & d are equal & greater than c");
+			}
+			else if(a>b&&b==d&&b==c) {
+				System.out.println("a is greater than b,c & d and b &c & d are equal ");
+			}
+			else if(a>b&&b==c&&b>d) {
+				System.out.println("a is greater than b and b & c are equal & greater than d");
+			}
+			else if(a>d&&b==c&&d>b) {
+				System.out.println("a is greater than d and d is greater than  b & c and b & c are equal ");
+			}
+			else if(a>b&&b==c&&b>d) {
+				System.out.println("a is greater than b and b & c are equal and greater than d ");
+			}
+			else if(a>c&&d==c&&d>b) {
+				System.out.println("a is greater than c and d & c are equal and greater than b ");
+			}
+			else if(a>b&&d==c&&b>d) {
+				System.out.println("a is greater than b & b is greater than c and c & d are equal ");
+			}
+			else if(a>c&&b==d&&c>d) {
+				System.out.println("a is greater than c and  c is greater than b and b & d are equal ");
+			}
+			else if(a>b&&b==d&&b>c) {
+				System.out.println("a is greater than b and b & d are equal and greater than c");
+			}
 			else  {
 				System.out.println("a is  greater than b & c & d");
 			}
@@ -119,6 +145,21 @@ public class program01 {
 			else if (b>c&&c>a&&a==d) {
 				System.out.println("b is greater than c & c is greater than a & a and d are equal");
 			}
+			else if (b==d&&b>a&&a==c) {
+				System.out.println("b & d are equal & greater than a & a and c are equal");
+			}
+			else if (b>a&&a==d&&a==c) {
+				System.out.println("b is greater than a and a & c &d are equal ");
+			}
+			else if (b>a&&a>c&&c==d) {
+				System.out.println("b is greater than a and a is greater than c and c & d are equal ");
+			}
+			else if (b>a&&a==d&&d>c) {
+				System.out.println("b is greater than a and a & d are equal and greater than c ");
+			}
+			else if (b>c&&c==d&&c>a) {
+				System.out.println("b is greater than c and c & d are equal & greater than a");
+			}
 			else  {
 				System.out.println("b is  greater than a & c & d");
 			}
@@ -155,6 +196,24 @@ public class program01 {
 			else if (c>b&&b==d&b>a) {//
 				System.out.println("c is greater than b & b and d are equal & greater than a");
 			}
+			else if (c==d&&c>b&&b==a) {
+				System.out.println("c & d are equal & greater than b and b & a are equal");
+			}
+			else if (c>a&&a==d&&d>b) {
+				System.out.println("c is greater than a and a & d are equal and greater than b");
+			}
+			else if (c>a&&a==b&&b>d) {
+				System.out.println("c is greater than a and a & b are equal and greater than d");
+			}
+			else if (c>a&&b==d&&a>b) {
+				System.out.println("c is greater than a and a is greater than b and b & d are equal");
+			}
+			else if (c>a&&a==d&&d==b) {
+				System.out.println("c is greater than a and a & b &d are equal ");
+			}
+			else if (c>d&&a==b&&d>b) {
+				System.out.println("c is greater than d and d is greater than a and a & b are equal ");
+			}
 			else {
 				System.out.println("c is  greater than a,b & d");
 			}
@@ -175,12 +234,25 @@ public class program01 {
 		else if(a>c&&c>b) {
 			System.out.println("d is greater than a & a is greater than c and c is greater than b");
 		}
+		else if(d>a&&a==b&&a==c) {
+			System.out.println("d is greater than a & a ,b & c are equal");
+		}else if(b>a&&a==c) {
+			System.out.println("d is greater than b & b is greater than a and a & c are equal");
+		}
+		else if(c>b&&c==a) {
+			System.out.println("d is greater than a & a and c are equal & greater than b");
+		}
+		else if(d>b&&b>a&&a==c) {
+			System.out.println("d is greater than b & b is greater than a & a and c are equal ");
+		}
 		else   {
 			System.out.println("d is greater than b & b is greater than c and c is greater than a");
 		}
 		}
 	}
-	}
+}
+
+	
 
 	
 	
