3d2689143fd620ff3c7b4ea836e04c1398220c79
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-23 18:57:15 +0530

    day09

--- src/day09/program01.java
+++ src/day09/program01.java
@@ -11,14 +11,14 @@ public class program01 {
 		    a=a+1;
 		}
 		System.out.println("********************************");
-	    a=a+1;
+	   
         double b=0.5;
         while(b>=0.1) {
         	System.out.println("....Good Morning....");
 		    b=b-0.1;
         }
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-	    a=a+1;
+	
         int c =-10;
         while(c>=-15) {
         	System.out.println("....Have a Nice Day ....");
