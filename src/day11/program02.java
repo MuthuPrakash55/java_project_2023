95d6c36ff2b5aa4bf9e540c4efc410502b414564
MuthuPrakash55 <123613944+MuthuPrakash55@users.noreply.github.com>
2023-02-24 20:20:59 +0530

    day11

---
+++ src/day11/program02.java
@@ -0,0 +1,21 @@
+package day11;
+
+public class program02 {
+
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--) {
+			for(int j=i;j>=1;j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+		System.out.println("****************");
+		for(int i=1;i<=5;i++) {
+			for(int j=i;j>=1;j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
