f1c1e55047c2971544549af9d524b2d4112a339b
MuthuPrakash <prakashmuthu17@gmail.com>
2023-02-28 10:07:39 +0530

    day14

---
+++ src/day14/primeno.java
@@ -0,0 +1,26 @@
+package day14;
+
+public class primeno {
+
+	public static void main(String[] args) {
+		int x=7;
+		boolean a=true;
+		for(int i=2;i<=x;i++) {
+			if(x%i==0) {
+				a=false;
+				break;
+			}
+		}
+		if(a) {
+			System.out.println("prime "+ x);
+		}
+		else {
+			System.out.println("not prime "+ x);
+		}
+				}
+			}
+		
+
+	
+
+
---
+++ src/day14/program02.java
@@ -0,0 +1,23 @@
+package day14;
+              //ARMSTRONG NUMBER
+public class program02 {
+
+	public static void main(String[] args) {
+		int  x1[]= {153, 370, 371, 507, 1634, 8208, 9476};
+	for(int x:x1) {
+		String y=String.valueOf(x);
+		int count=y.length();
+		double z=0;
+		for(int i=0;i<count;i++) {
+			z=Math.pow(Integer.parseInt(y.charAt(i)+""), count)+z;
+		}
+		if(z==x) {
+			System.out.println("armstrong no  "+x);
+		}
+	}
+	
+	
+	
+	}
+
+}
