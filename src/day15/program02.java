0cfd41888dcc64ec6e096c17c7ef8fe9b71fc040
MuthuPrakash <prakashmuthu17@gmail.com>
2023-03-02 10:29:16 +0530

    day15

---
+++ src/day15/program02.java
@@ -0,0 +1,45 @@
+package day15;
+import java.util.Arrays;
+import java.util.HashSet;
+public class program02 {
+	public static void main(String[] args) {
+		HashSet<Integer> x=new HashSet<>();
+        x.add(10);
+        x.add(20);
+        x.add(30);
+        x.add(40);
+        System.out.println(x);
+        x.add(40);
+        x.add(10);
+        x.add(80);
+        System.out.println(x);
+        x.remove(4);
+        System.out.println(x);
+      //  System.out.println(x.get(3));//Get function
+        System.out.println(x.size());//Size function
+        Integer y[]= {40,90,99,88,33};
+        x.addAll(Arrays.asList(y));//convert arrays to list & add all values of y in x 
+        System.out.println(x);
+        Integer z[]= {10,30,99};
+        x.removeAll((Arrays.asList(z)));//convert arrays to list & remove all values of z in x
+        System.out.println(x);
+        System.out.println(x.contains(80));//to verify the value is in x or not
+        System.out.println(x.contains(666));//to verify the value is in x or not
+        Integer a[]= {20,40,90};
+        System.out.println("a - "+x.containsAll(Arrays.asList(a)));//to verify all the values of a is in x or not 
+	    Integer b[]= {10,20,99};
+	    System.out.println("b - "+x.containsAll(Arrays.asList(b)));//to verify all the values of b is in x or not 
+	   // x.add(3,66);//add values in between the indexes
+	   // System.out.println(x);
+	   // x.set(3,666);//replace the values in the index
+	   // System.out.println(x);
+	    System.out.println(x.isEmpty());
+	    x.clear();
+	    System.out.println(x.isEmpty());
+	    System.out.println(x);
+	
+	
+
+	}
+
+}
